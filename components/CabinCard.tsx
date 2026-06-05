920e0313209e7cc6a5d2b9c9e92e8a4ffc1f0f7c
Bnstrh <benamaroh@gmail.com>
2026-06-05 11:33:30 +0700

    integrating supabase

--- components/CabinCard.tsx
+++ components/CabinCard.tsx
@@ -5,12 +5,13 @@ import images from "@/public/about2.webp";
 export default function CabinCard({ index }: { index: number }) {
   return (
     <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-      <div className="flex h-[500px] items-center justify-center bg-amber-200">
-        <div className="relative grid h-full w-full grid-rows-2">
-          <div className="relative">
+      <div className="flex h-[500px] items-center justify-center border border-primary-accent-200 bg-primary-base">
+        <div className="relative grid h-full w-full grid-rows-[65%_1fr] space-y-2">
+          <div className="relative bg-blue-400">
             {/* <Image src={images} alt="" fill className="object-cover" /> */}
+            IMAGE
           </div>
-          <p>{index}</p>
+          <div className="px-2">{index}</div>
         </div>
       </div>
     </CarouselItem>
