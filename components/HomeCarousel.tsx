b1077960b0a7f63a27da3e58995fcabad4605c47
Bnstrh <benamaroh@gmail.com>
2026-06-08 10:00:57 +0700

    styles: homepage-aboutsection

--- components/HomeCarousel.tsx
+++ components/HomeCarousel.tsx
@@ -10,7 +10,7 @@ export default async function HomeCarousel() {
       <div className="mb-5 flex items-center justify-between gap-y-2 px-6 font-decorative sm:flex-col sm:justify-center sm:px-8 sm:text-2xl">
         <h1 className="text-2xl">Our Cabins</h1>
         <div>
-          <button className="flex h-8 items-center justify-center bg-primary-green-500 p-2 text-xs text-primary-base hover:bg-primary-accent-900 sm:h-8 sm:text-sm">
+          <button className="flex h-8 cursor-pointer items-center justify-center bg-primary-green-500 p-2 text-xs text-primary-base hover:bg-primary-accent-900 sm:h-8 sm:text-sm">
             Discover all cabins
           </button>
         </div>
